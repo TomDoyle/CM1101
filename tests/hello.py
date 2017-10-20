127f955cd1794e16f87f1530da99c571c10105b7
Tom Doyle <32750818+TomDoyle@users.noreply.github.com>
2017-10-20 10:21:09 +0100

    Add files via upload

---
+++ tests/hello.py
@@ -0,0 +1,10 @@
+print("Hello")
+<<<<<<< HEAD
+print("World")
+print("More Changes")
+print("Still more changes")
+=======
+print("Changes in another")
+print("World")
+print("More Changes")
+>>>>>>> a7ae2b75e71b8b25a58f21136fe0df3991291c59
